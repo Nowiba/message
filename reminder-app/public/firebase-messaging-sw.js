4c31d8d3254b4658d07b9e0d7e1b911fe64e70bd
Nowiba <mohamedlabido2646@gmail.com>
2025-07-01 23:25:38 +0100

    Update firebase-messaging-sw.js

--- reminder-app/public/firebase-messaging-sw.js
+++ reminder-app/public/firebase-messaging-sw.js
@@ -4,7 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
 firebase.initializeApp({
   apiKey: "AIzaSyBMJuAmIAqDUXhZcGBHq8CszhJL92VaZ64",
   authDomain: "information-project1.firebaseapp.com",
-  databaseURL: "https://information-project1-default-rtdb.firebaseio.com",
   projectId: "information-project1",
   storageBucket: "information-project1.appspot.com",
   messagingSenderId: "1098270976013",
@@ -14,11 +13,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('[SW] Received background message', payload);
-  const notificationTitle = payload.notification?.title || 'Appointment Reminder';
-  const notificationOptions = {
-    body: payload.notification?.body || 'You have a new notification',
-    icon: '/icons/icon-192x192.png'
-  };
-  return self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log('Received background message', payload);
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, { body, icon: '/icons/icon-192x192.png' });
 });
